c7a2a36aa414f3332dce2b9926cb38f996c62a95
Seige Poteau <spoteau@gmail.com>
2025-06-20 15:48:45 -0400

    first commit

---
+++ src/Data.js
@@ -0,0 +1,86 @@
+let shopItemsData = [
+    {
+        id: "item1",
+        name: "Savon au miel",
+        price: 3.50,
+        desc: "Un savon naturel au miel pour une peau douce et hydratée.",
+        img: "images_soap/honey-7330657_1280.jpg"
+    },
+    {
+        id: "item2",
+        name: "Savon à l'aloe vera",
+        price: 4.50,
+        desc: "Apaise les irritations et régénère la peau en douceur.",
+        img: "images_soap/aloe.jpg"
+    },
+    {
+        id: "item3",
+        name: "Savon au charbon actif",
+        price: 4,
+        desc: "Nettoyage en profondeur des pores pour les peaux grasses.",
+        img: "images_soap/charcoal.jpg"
+    },
+    {
+        id: "item4",
+        name: "Savon à la lavande",
+        price: 2,
+        desc: "Détente assurée grâce au parfum apaisant de lavande.",
+        img: "images_soap/lavender.jpg"
+    },
+    {
+        id: "item5",
+        name: "Savon à la menthe",
+        price: 3,
+        desc: "Sensation de fraîcheur revigorante pour bien commencer la journée.",
+        img: "images_soap/mint.jpg"
+    },
+    {
+        id: "item6",
+        name: "Savon au curcuma",
+        price: 4.50,
+        desc: "Un teint lumineux grâce aux propriétés anti-inflammatoires du curcuma.",
+        img: "images_soap/turmeric.jpg"
+    },
+    {
+        id: "item7",
+        name: "Savon au lait",
+        price: 5.00,
+        desc: "Hydratation intense pour les peaux sensibles et sèches.",
+        img: "images_soap/goatmilk.jpg"
+    },
+    {
+        id: "item8",
+        name: "Savon à la rose",
+        price: 2,
+        desc: "Parfum floral délicat et soin nourrissant pour la peau.",
+        img: "images_soap/rose.jpg"
+    },
+    {
+        id: "item9",
+        name: "Savon café",
+        price: 1.50,
+        desc: "Élimine les peaux mortes et stimule la circulation sanguine.",
+        img: "images_soap/coffee.jpg"
+    },
+    {
+        id: "item10",
+        name: "Savon à l’argile",
+        price: 5,
+        desc: "Purifie et matifie les peaux mixtes à grasses.",
+        img: "images_soap/clay.jpg"
+    },
+    {
+        id: "item11",
+        name: "Savon canelle et miel",
+        price: 3,
+        desc: "Éclat naturel et action antibactérienne pour une peau nette.",
+        img: "images_soap/canellemiel.jpg"
+    },
+    {
+        id: "item12",
+        name: "Savon au karité",
+        price: 4,
+        desc: "Riche en beurre de karité pour une peau nourrie et protégée.",
+        img: "images_soap/shea.jpg"
+    }
+];
